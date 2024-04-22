196b7923a5e1f55f9a4c7dd0401b5cdf640f2ded
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-04-22 09:42:51 -0300

    Add files via upload

---
+++ LISTA 4.12/LISTA 4.12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA_4._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double b;
+            Double a;
+            Double area;
+
+            Console.Write("digite a base do triangulo: ");
+            b= Double.Parse(Console.ReadLine());
+
+            Console.Write("digite a altura do triagulo: ");
+            a= Double.Parse(Console.ReadLine());
+
+            area= b*a /2;
+
+            Console.Write("valor da area do triangulo é: ");
+            Console.WriteLine(area);
+
+        }
+    }
+}
