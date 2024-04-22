0fd556d1eb601f10408ca3379483b15f09b6a34d
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-04-22 09:45:47 -0300

    Add files via upload

---
+++ LISTA 8.12/LISTA 8.12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA_8._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double celsius;
+            Double fahrenheit;
+
+            Console.Write("digite o valor de graus em celsius: ");
+            celsius= Double.Parse(Console.ReadLine());
+
+            fahrenheit= celsius * 1.8 + 32;
+
+            Console.WriteLine("a temperatura em fahrenheit é: ");
+            Console.WriteLine(fahrenheit);
+
+        }
+    }
+}
