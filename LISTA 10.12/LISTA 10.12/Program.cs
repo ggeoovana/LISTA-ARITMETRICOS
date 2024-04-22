0ac7b9be6298c43785a806774451f1c7b792b550
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-04-22 09:46:33 -0300

    Add files via upload

---
+++ LISTA 10.12/LISTA 10.12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA_10._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolar;
+            double x;
+            double reais;
+
+            Console.Write("digite o valor da cotação do dolar: ");
+            dolar= double.Parse(Console.ReadLine());
+
+            Console.Write("digite a quantidade de dolares: ");
+            x= double.Parse(Console.ReadLine());
+
+            reais= dolar * x;
+
+            Console.Write("o valor em reais é: ");
+            Console.WriteLine(reais);
+            
+        }
+    }
+}
