a0d2d4b5f972e5ae904245b59326f89857951daf
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-04-22 09:46:10 -0300

    Add files via upload

---
+++ LISTA 9.12/LISTA 9.12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA_9._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double d;
+            Double raio;
+            Double area;
+
+            Console.Write("digite o valor do diametro: ");
+            d= Double.Parse (Console.ReadLine());
+
+            raio= d/2;
+
+            area= 3.14 * Math.Pow(raio, 2);
+
+            Console.WriteLine("a area do circulo é: ");
+            Console.WriteLine(area);
+
+        }
+    }
+}
