77c459da223e2b1fc81b10877bbd5952f08457d5
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-04-22 09:46:55 -0300

    Add files via upload

---
+++ LISTA 11.12/LISTA 11.12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA_11._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double x;
+            Double y;
+            Double resultado;
+
+            Console.Write("digite o valor de x: ");
+            x= Double.Parse(Console.ReadLine());
+
+            Console.Write("digite o valor de y: ");
+            y= Double.Parse(Console.ReadLine());
+
+            resultado= Math.Pow(x, y);
+
+            Console.Write("o valor de x elevado ao valor de y é igual a: ");
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
