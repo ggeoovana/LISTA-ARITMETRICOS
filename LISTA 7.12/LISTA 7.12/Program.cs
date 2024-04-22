f64c6531fc19cba9246695a44ab01a5517c4de27
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-04-22 09:44:23 -0300

    Add files via upload

---
+++ LISTA 7.12/LISTA 7.12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA_7._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double milhas;
+            Double km;
+
+            Console.Write("digite o valor em milhas: ");
+            milhas= Double.Parse(Console.ReadLine());
+
+            km= milhas * 1852;
+
+            Console.WriteLine("o valor em quilometros é: ");
+            Console.WriteLine(km);
+        }
+    }
+}
