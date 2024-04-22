c770ea9f095b725abf3adefb6ea02b272982c873
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-04-22 09:47:15 -0300

    Add files via upload

---
+++ LISTA 12.12/LISTA 12.12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA_12._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double produto1;
+            double produto2;
+            double produto3;
+            double produto4;
+            double produto5;
+            double total;
+            double pagamento;
+            double troco;
+
+            Console.Write("digite o valor do produto: ");
+            produto1= double.Parse(Console.ReadLine());
+
+            Console.Write("digite o valor do produto: ");
+            produto2= double.Parse(Console.ReadLine());
+
+            Console.Write("digite o valor do produto: ");
+            produto3= double.Parse(Console.ReadLine());
+
+            Console.Write("digite o valor do produto: ");
+            produto4= double.Parse(Console.ReadLine());
+
+            Console.Write("digite o valor do produto: ");
+            produto5= double.Parse(Console.ReadLine());
+
+            total= produto1 + produto2 + produto3 + produto4 + produto5;
+
+            Console.Write("total: ");
+            Console.WriteLine(total);
+
+            Console.Write("digite o valor do pagamento: ");
+            pagamento= double.Parse(Console.ReadLine());
+
+            troco= pagamento - total;
+
+            Console.Write("o valor do troco é: ");
+            Console.WriteLine(troco);
+
+        }
+    }
+}
