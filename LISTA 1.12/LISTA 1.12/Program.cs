3e5867819b38131c8c3fca3d19c25192db647ce6
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-04-22 09:41:33 -0300

    Add files via upload

---
+++ LISTA 1.12/LISTA 1.12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA_1._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int a;
+            int area;
+
+            Console.Write("digite a base do retangulo: ");
+            b= int.Parse(Console.ReadLine());
+
+            Console.Write("digite a altura do retangulo: ");
+            a= int.Parse(Console.ReadLine());
+
+            area= b*a;
+
+            Console.Write("a area do retangulo é: ");
+            Console.WriteLine(area);
+
+        }
+    }
+}
