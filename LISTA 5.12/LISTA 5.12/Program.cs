cc5776c68f42dc03a2a8bdd1868564d31ecf375d
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-04-22 09:43:22 -0300

    Add files via upload

---
+++ LISTA 5.12/LISTA 5.12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA_5._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double primeiro;
+            Double segundo;
+            Double terceiro;
+            Double quarto;
+            Double resultado;
+
+            Console.Write("digite o primeiro valor: ");
+            primeiro= Double.Parse(Console.ReadLine());
+
+            Console.Write(" digite o segundo valor: ");
+            segundo= Double.Parse(Console.ReadLine());
+
+            Console.Write("digite o terceiro valor: ");
+            terceiro= Double.Parse(Console.ReadLine());
+
+            Console.Write("digite o quarto valor: ");
+            quarto= Double.Parse(Console.ReadLine());
+           
+            resultado= (primeiro + segundo + terceiro + quarto) / 4;
+
+            Console.WriteLine("a media aritmetrica dos quatro valores digitados é: ");
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
