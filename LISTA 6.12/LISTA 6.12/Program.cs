643663d96b30f27083936d1fe3bcbe840c4fcd91
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-04-22 09:43:54 -0300

    Add files via upload

---
+++ LISTA 6.12/LISTA 6.12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA_6._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double valor1;
+            Double valor2;
+            Double resultado;
+
+            Console.Write("digite o primeiro valor: ");
+            valor1= Double.Parse(Console.ReadLine());
+
+            Console.Write("digite o segundo valor: ");
+            valor2= Double.Parse(Console.ReadLine());
+
+            resultado= Math.Sqrt(valor1*valor2);
+
+            Console.WriteLine("o valor da media geometrica é: ");
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
