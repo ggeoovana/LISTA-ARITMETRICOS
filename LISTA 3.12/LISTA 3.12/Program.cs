8bc43d0ac43cc9e677828a9b503ac7eaa8c9c492
ggeoovana <166414582+ggeoovana@users.noreply.github.com>
2024-04-22 09:42:28 -0300

    Add files via upload

---
+++ LISTA 3.12/LISTA 3.12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA_3._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double d;
+            Double area;
+
+            Console.Write("digite a diagonal do quadrado: ");
+            d= Double.Parse(Console.ReadLine());
+
+            area= d*d;
+
+            Console.Write(" a area do quadrado é: ");
+            Console.WriteLine(area);
+
+
+
+        }
+    }
+}
